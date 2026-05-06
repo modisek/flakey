f0bec2d9bd263260c67705713250b9605d198665
kgosietsile <warlockmodise@gmail.com>
2026-05-06 22:55:32 +0200

    fix:hadware url

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-hardware.url = "github:NixOS/nix-hardware/master";
+    nix-hardware.url = "github:NixOS/nix-hardware";
     # chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
